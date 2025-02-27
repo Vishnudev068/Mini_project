c67d65067d1f288614773112e9abf24452f6d182
vishnu <vishnusudevan68@gmail.com>
2025-02-27 18:38:13 +0530

    firestore

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -1,14 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirestoreServices {
-  final CollectionReference notes = FirebaseFirestore.instance.collection(
-    'notes',
-  );
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+ 
+  // Future<void> addNote(String note) {
+  //   return notes.add({'note': note, 'time': Timestamp.now()});
+  // }
+  // Stream<QueryDocumentSnapshot> getList(){
 
-  Future<void> addNote(String note) {
-    return notes.add({
-      'note':note,
-      'time':Timestamp.now()
-    });
-  }
+     
+    
+  // }
+
+  Stream<QuerySnapshot> getList(String prefix) {
+  return FirebaseFirestore.instance
+      .collection('doctors') // Replace with your Firestore collection
+      .where('name', isGreaterThanOrEqualTo: prefix)
+      .where('name', isLessThan: prefix + '\uf8ff') // Ensures prefix filtering
+      .snapshots();
+}
+
 }
